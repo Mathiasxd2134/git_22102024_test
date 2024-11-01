6eaf18e19826a5c6d9e859797917debb4124c332
Mathiasxd2134 <mathias.rm.9@gmail.com>
2024-11-01 13:05:52 -0500

    Update desde ubuntu

--- hola_mundo.py
+++ hola_mundo.py
@@ -2,6 +2,9 @@
 def saludo():
     print("Hola,mundo!")
     print("SESION 2  DE GIT CAMBIOS EN EL REPOSITORIO REMOTO DESDE WINDOWS")
+    print("Esta línea de codigo fue desarrollada en la PC ubuntu")
+    print("Trabajamos en ubuntu desde 9:28pm-9:30pm")
+
     
 
 if __name__ == '__main__':
